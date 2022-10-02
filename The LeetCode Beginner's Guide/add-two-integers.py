20370314443bccff98b55c3ab58193b60a9cbb2c
Md. Nazmul Hossain <30841411+MdNazmul9@users.noreply.github.com>
2022-10-02 07:29:17 +0600

    [2235. Add Two Integers](https://leetcode.com/problems/add-two-integers/submissions/)

---
+++ The LeetCode Beginner's Guide/add-two-integers.py
@@ -0,0 +1,5 @@
+# [2235. Add Two Integers](https://leetcode.com/problems/add-two-integers/submissions/)
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
