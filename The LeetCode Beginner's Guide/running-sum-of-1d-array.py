6ff1e78a5532b2bb01ad6cefc8d2b0f57c8d5de6
Md. Nazmul Hossain <30841411+MdNazmul9@users.noreply.github.com>
2022-10-02 08:42:02 +0600

    Update running-sum-of-1d-array.py

--- The LeetCode Beginner's Guide/running-sum-of-1d-array.py
+++ The LeetCode Beginner's Guide/running-sum-of-1d-array.py
@@ -3,9 +3,7 @@
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        sum_nums = []
-        sm = 0
-        for i in nums:
-            sm +=i
-            sum_nums.append(sm)
-        return sum_nums
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        return nums
+    
