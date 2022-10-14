2a75574b3cf303fa62914ae08d2c9f1bc5441fc9
MdNazmul9 <nazmul.cse.pust@gmail.com>
2022-10-14 22:53:36 +0600

    biginners guide completed

--- The LeetCode Beginner's Guide/LinkedList.py
+++ The LeetCode Beginner's Guide/LinkedList.py
@@ -28,10 +28,10 @@ class LinkedList:
         
 if __name__=="__main__":
     ll = LinkedList()
-    # ll.insert_at_beginning(5)
-    # ll.insert_at_beginning(15)
-    # ll.insert_at_beginning(10)
-    # ll.insert_at_beginning(21)
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(15)
+    ll.insert_at_beginning(10)
+    ll.insert_at_beginning(21)
     ll.print()
         
     
