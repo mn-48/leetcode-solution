c9fde0f4167a786288afdec93e27d4a79a1d63fb
Md. Nazmul Hossain <30841411+nazmul-cse48@users.noreply.github.com>
2023-03-24 08:05:04 +0600

    Time: 123 ms, Memory: 16.9 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -2,16 +2,22 @@
 from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for num in nums:
-            res ^= num
-        return res
-#         c = Counter(nums)
-#         # print(c)
+        return reduce(operator.xor, nums)
         
-#         set_nums = set(nums)
-#         for i in set_nums:
-#             if c[i] == 1:
-#                 return i
+    #--------------------------
+        return reduce(lambda x, y: x ^ y, nums)
+    #--------------------------
+        # res = 0
+        # for num in nums:
+        #     res ^= num
+        # return res
+    #--------------------------
+        # c = Counter(nums)
+        # print(c)
+        
+        # set_nums = set(nums)
+        # for i in set_nums:
+        #     if c[i] == 1:
+        #         return i
                 
         
